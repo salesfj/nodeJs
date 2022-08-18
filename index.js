fb77d02c69a98e1437c04e0dde97b60166163c4c
salesfj <saleswebd@gmail.com>
2022-08-18 20:58:15 -0300

    18/08/2022 20:58

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ console.log("3 - Mensagem de commit");
 console.log("4 - Confirmação");
 console.log("5 - Sincronizar alterações");
 console.log("6 - 18/08/2022 20:44");
+console.log("git add index.js");
 console.log("git commit -m");
 console.log("git push origin master");
