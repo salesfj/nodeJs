da05d2efc287b21e066fdcca507c99881c105dc8
salesfj <saleswebd@gmail.com>
2022-08-18 21:30:26 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,9 @@
+console.log("1 - Alteração do arquivo");
+console.log("2 - Salvar arquivo");
+console.log("3 - Mensagem de commit");
+console.log("4 - Confirmação");
+console.log("5 - Sincronizar alterações");
+console.log("6 - 18/08/2022 20:44");
+console.log("git add index.js");
+console.log("git commit -m");
+console.log("git push origin master");
